7b2b35bba470d50c68fca54730f48c07a07a040a
AbdullahAOT <abdallahabuhalima.1.jo@gmail.com>
2024-05-19 06:55:15 +0300

    Lab16 firdt commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import React from 'react';
+import Home from "./Components/Home/Home.js";
+
+function App() {
+  return(
+    <>
+    <Home></Home>
+    </>
+  );
+}
+
+export default App;
